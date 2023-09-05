3410116aa6b551aaf81574a962a726c700106e0c
cam <cammo789@gmail.com>
2023-09-05 03:14:14 +0000

    JS101 lesson 2 calculator refactor

--- backEnd/JS101/lesson_2/calculator.js
+++ backEnd/JS101/lesson_2/calculator.js
@@ -6,30 +6,56 @@
 
 let rlSync = require('readline-sync');
 
-console.log(`Welcome to The Calculator!`);
+function prompt(msg) {
+  console.log(`=> ${msg}`);
+}
 
-let num1 = Number(rlSync.question(`What is the first number?: `));
-let num2 = Number(rlSync.question(`What is the second number?: `));
-let operation = rlSync.question(`What operation would you like to perform?\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n`);
+function invalidNumber(number) {
+  // we're looking for invalid numbers without whitespace
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt(`Welcome to The Calculator!`);
+
+let num1 = rlSync.question(prompt(`What is the first number?: `));
+
+while (invalidNumber(num1)) {
+  prompt(`Hmmm... that doesn't look like a valid number.`);
+  num1 = rlSync.question();
+}
+
+let num2 = rlSync.question(prompt(`What is the second number?: `));
+
+while (invalidNumber(num2)) {
+  prompt(`Hmmm... that doesn't look like a valid number.`);
+  num2 = rlSync.question();
+}
+
+let operation = rlSync.question(prompt(`What operation would you like to perform?\n\n1) Add\n2) Subtract\n3) Multiply\n4) Divide\n`));
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt(`Please choose either 1, 2, 3, or 4`);
+  operation = rlSync.question();
+}
 
 let output;
 
-switch(operation) {
+switch (operation) {
   case '1':
-    output = num1 + num2;
+    output = Number(num1) + Number(num2);
     break;
   case '2':
-    output = num1 - num2;
+    output = Number(num1) - Number(num2);
     break;
   case '3':
-    output = num1 * num2;
+    output = Number(num1) * Number(num2);
     break;
   case '4':
-    output = num1 / num2;
+    output = Number(num1) / Number(num2);
     break;
   default:
     console.log(`Error`);
     break;
 }
 
-console.log(`The result is ${output}`)
+prompt(`The result is ${output}`);
