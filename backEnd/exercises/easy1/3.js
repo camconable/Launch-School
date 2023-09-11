0539ee04236e2e9d54322a86173606da2c66e1ea
cam <cammo789@gmail.com>
2023-09-11 02:54:43 +0000

    JS101 exercises thru easy 4

---
+++ backEnd/exercises/easy1/3.js
@@ -0,0 +1,8 @@
+// log all even numbers from 1 to 99, inclusive, to the console,
+// with each number on a separate line
+
+let num = 2;
+do {
+  console.log(num);
+  num += 2;
+} while (num <= 99);
