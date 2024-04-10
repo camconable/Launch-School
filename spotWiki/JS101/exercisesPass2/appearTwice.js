6c48f09df8aa212212e313ae7e2379fa299adf18
cam <cammo789@gmail.com>
2024-04-10 02:03:51 +0000

    JS101 AI exercises Pass 1: longestPalindrome_rev1

---
+++ spotWiki/JS101/exercisesPass2/appearTwice.js
@@ -0,0 +1,9 @@
+/* eslint-disable max-len */
+// Create a function that takes an array as an argument and returns an new array containing all of the array elements that appears twice, for example:
+
+
+appearTwice([1, 2, undefined, 'hello', 2, undefined]); //=> [2, undefined];
+
+// input: array
+// output: new array, containing only element that appear exactly 2 times
+
